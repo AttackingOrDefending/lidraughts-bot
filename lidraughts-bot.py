4291581fbba06ad5d8d9acfefa29b1a37600dc2e
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2021-07-14 17:40:06 +0300

    Update lidraughts-bot.py

--- lidraughts-bot.py
+++ lidraughts-bot.py
@@ -230,6 +230,13 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
     variant = parse_variant(game.variant_name)
     engine = engine_factory(variant)
     conversation = Conversation(game, engine, li, __version__, challenge_queue)
+    
+    class SendLine:
+        def __init__(self, room):
+            self.room = room
+    opponent = game.black.name if game.white.name == user_profile["username"] else game.white.name
+    conversation.send_reply(SendLine('player'), f'Good Luck {opponent}')
+    conversation.send_reply(SendLine('spectator'), f'Good Luck {opponent}')
 
     logger.info("+++ {}".format(game))
 
