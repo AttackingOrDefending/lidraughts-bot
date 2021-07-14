5701b2919be260ab030428a50c92d2d81f3e7168
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2021-07-14 17:51:29 +0300

    Update lidraughts-bot.py

--- lidraughts-bot.py
+++ lidraughts-bot.py
@@ -235,8 +235,8 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
         def __init__(self, room):
             self.room = room
     opponent = game.black.name if game.white.name == user_profile["username"] else game.white.name
-    conversation.send_reply(SendLine('player'), f'Good Luck {opponent}')
-    conversation.send_reply(SendLine('spectator'), f'Good Luck {opponent}')
+    conversation.send_reply(SendLine('player'), f'Good Luck @{opponent}')
+    conversation.send_reply(SendLine('spectator'), f'Good Luck @{opponent}')
 
     logger.info("+++ {}".format(game))
 
@@ -322,6 +322,9 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
         except StopIteration:
             break
 
+    conversation.send_reply(SendLine('player'), f'Good Game')
+    conversation.send_reply(SendLine('spectator'), f'Good Game')
+    
     engine.stop()
     engine.quit()
     if ponder_thread is not None:
