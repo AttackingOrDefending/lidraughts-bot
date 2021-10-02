c00e9bb4480b6327b904768b2d5f70f6dd4d44f3
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2021-10-02 12:06:29 +0300

    pydraughts now runs command 'hub' automatically

--- engine_wrapper.py
+++ engine_wrapper.py
@@ -98,7 +98,6 @@ class HubEngine(EngineWrapper):
         self.last_move_info = {}
         self.go_commands = options.pop("go_commands", {}) or {}
         self.engine = hub_engine(commands)
-        self.engine.uci()
 
         if 'bb-size' in options and options['bb-size'] == 'auto':
             if 'variant' in options and options['variant'] != 'normal':
