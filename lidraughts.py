b9f2977b9357b931a4c040c313fb0937329bd312
AttackingOrDefending <40605232+AttackingOrDefending@users.noreply.github.com>
2022-07-28 16:08:46 +0300

    Handle rate limit status code (ShailChoksi/lichess-bot#510)
    * Wait 1 minute after status code 429
    
    A status code of 429 indicates that lichess is rate-limiting the bot, so
    API communication is effectively cut off. Wait one minute before trying
    to communicate again.
    
    Reference: https://lichess.org/api#section/Introduction/Rate-limiting
    
    * Handle exception when creating challenge
    
    * Log all escaping exceptions before quitting
    
    Just in case there is an exception that causes lichess-bot to quit, log
    this last exception before quitting.

--- lidraughts.py
+++ lidraughts.py
@@ -4,6 +4,7 @@ from requests.exceptions import ConnectionError, HTTPError, ReadTimeout
 from http.client import RemoteDisconnected
 import backoff
 import logging
+import time
 
 ENDPOINTS = {
     "profile": "/api/account",
@@ -20,6 +21,17 @@ ENDPOINTS = {
     "resign": "/api/bot/game/{}/resign",
     "export": "/game/export/{}"
 }
+
+
+logger = logging.getLogger(__name__)
+
+
+def rate_limit_check(response):
+    if response.status_code == 429:
+        logger.warning("Rate limited. Waiting 1 minute until next request.")
+        time.sleep(60)
+        return True
+    return False
 
 
 # docs: https://lidraughts.org/api
@@ -49,7 +61,7 @@ class Lidraughts:
         logging.getLogger("backoff").setLevel(self.logging_level)
         url = urljoin(self.baseUrl, path)
         response = self.session.get(url, timeout=2, params=params)
-        if raise_for_status:
+        if rate_limit_check(response) or raise_for_status:
             response.raise_for_status()
         return response.text if get_raw_text else response.json()
 
@@ -60,11 +72,12 @@ class Lidraughts:
                           giveup=is_final,
                           backoff_log_level=logging.DEBUG,
                           giveup_log_level=logging.DEBUG)
-    def api_post(self, path, data=None, headers=None, params=None):
+    def api_post(self, path, data=None, headers=None, params=None, raise_for_status=True):
         logging.getLogger("backoff").setLevel(self.logging_level)
         url = urljoin(self.baseUrl, path)
         response = self.session.post(url, data=data, headers=headers, params=params, timeout=2)
-        response.raise_for_status()
+        if rate_limit_check(response) or raise_for_status:
+            response.raise_for_status()
         return response.json()
 
     def get_game(self, game_id):
